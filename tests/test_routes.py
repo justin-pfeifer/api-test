e5a939ebee7435b29d034490851b519a21d77acd
justin-pfeifer <justin.pfeifer@protonmail.com>
2022-02-26 09:23:43 -0600

    Added Test

---
+++ tests/test_routes.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+def test_main():
+    response = client.get('/api/users/')
+    assert response.status_code == 200
+
+    response = client.get('/api/users/1')
+    assert response.status_code == 200
